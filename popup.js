5284a28e243c1c7918e62568a3fd83f2a1155b18
ambarytl <ambar.ytl@gmail.com>
2019-08-01 23:17:42 +0530

    🔤change font

--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-	
-	chrome.tabs.executeScript(tabs[0].id, {
-		file: 'contentscript.js'
-	},function()
-	{
-		chrome.tabs.sendMessage(tabs[0].id, 'cgpa', function(response) {
-			if(response)
-			document.getElementById('cgpa').textContent = `CGPA: ${response.cgpa}`;
-			else{
-				document.getElementById('cgpa').textContent = 'Some error occured';}
-		});
-
-	});
+	chrome.tabs.executeScript(
+		tabs[0].id,
+		{
+			file: 'contentscript.js'
+		},
+		function() {
+			chrome.tabs.sendMessage(tabs[0].id, 'cgpa', function(response) {
+				if (response) document.getElementById('cgpa').textContent = `CGPA:${response.cgpa}`;
+				else {
+					document.getElementById('cgpa').textContent = 'Some error occured';
+				}
+			});
+		}
+	);
 });
