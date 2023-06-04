88ab9ebd776b528e38414640be2d988d02d75000
aishikanandi <aishikanandi@gmail.com>
2023-06-04 22:42:07 +0530

    Fixed for M.tech, M.Sc

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 const courseRowSelector = '.hierarchyLi.dataLi.tab_body_bg:not(.hierarchyHdr)';
 const gradePointMap = {
 	'A+': 10,
-	A: 10,
+	'A': 10,
 	'A-': 9,
 	B: 8,
 	'B-': 7,
@@ -12,9 +12,10 @@ const gradePointMap = {
 	FR: 0,
 	F: 0,
 	I: 0,
-	'': 0,
+	' ': 0,
+	'':0
 };
-
+var gradeArray=[];
 const round = n => Math.round(n * 100) / 100;
 
 const getColumnText = (node, column) =>
@@ -30,6 +31,8 @@ const parseCourse = node => ({
 	credits: parseFloat(getColumnText(node, 3)),
 	electiveType: getColumnText(node, 5),
 	grade: getColumnText(node, 8),
+	courseName: getColumnText(node, 2),
+	courseDate: getColumnText(node, 9)
 });
 
 const parseSemester = semNode =>
@@ -45,17 +48,20 @@ const sanitizeSemesters = semesters => {
 	let coursesSeen = new Set();
 
 	const storeFinalGrades = semester =>
-		semester.forEach(course => (courseGrades[course.code] = course.grade));
+		semester.forEach(course => {if(course.courseName!=="THESIS"){courseGrades[course.code] = course.grade}else{
+			courseGrades[course.code+course.courseDate] = course.grade;
+		}});
 
 	semesters.forEach(storeFinalGrades);
 
-	const mutateCoursesWithImprovements = course =>
-		coursesSeen.has(course.code)
+	const mutateCoursesWithImprovements = (course) =>{
+		if(course.courseName!=="THESIS"){
+		return coursesSeen.has(course.code)
 			? {}
 			: coursesSeen.add(course.code) && {
 					...course,
 					grade: courseGrades[course.code],
-			  }; // Update the grades of first attempt for a course and empty the 'improvement' attempts
+			  }} else return course; }; // Update the grades of first attempt for a course and empty the 'improvement' attempts
 
 	const sanitizeSemester = semester => {
 		return semester
@@ -83,17 +89,22 @@ function calculateCGPA() {
 			semesterCredit = semester.reduce(
 				(sum, course) => sum + (course.grade ? course.credits : 0),
 				0,
-			),
-			semesterPoints = semester.reduce(
-				(sum, course) => sum + gradePointMap[course.grade] * course.credits,
-				0,
-			),
-			sgpa = round(semesterPoints / semesterCredit);
-
+			);
+		let semesterPoints=0;	
+		for(var i=0; i<semester.length; i++){
+			if(semester[i].courseName==="THESIS"){
+				gradeArray= semester[i].grade.split(' ');
+				var thesisSum= gradeArray.reduce((sum,grade)=>sum+gradePointMap[grade]*6,0);
+				semesterPoints+=thesisSum;
+			}else{
+				semesterPoints+=gradePointMap[semester[i].grade]*semester[i].credits;
+			}
+		}
 		if (isComplete) {
 			totalCredit += semesterCredit;
 			totalPoints += semesterPoints;
 		}
+		sgpa = round(semesterPoints / semesterCredit);
 		sgpas.unshift(sgpa);
 	});
 	const cgpa = round(totalPoints / totalCredit);
@@ -106,3 +117,4 @@ function calculateCGPA() {
 browser.runtime.onMessage.addListener(async request => {
 	if (request === 'grade-data') return calculateCGPA();
 });
+console.log(gradeArray);
